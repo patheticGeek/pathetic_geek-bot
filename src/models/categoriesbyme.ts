8304fe982ad8a3e644416c43b7d971355c7a3d41
patheticGeek <geekpathetic@gmail.com>
2021-05-10 19:53:41 +0530

    ✨ Add categories command

---
+++ src/models/categoriesbyme.ts
@@ -0,0 +1,34 @@
+import { DataTypes, Sequelize, Model } from "sequelize";
+
+interface CategoriesByMeAttributes {
+  name: string;
+  serverID: string;
+  channelID: string;
+  id: string;
+  userID: string;
+}
+
+interface CategoriesByMeInstance extends Model<CategoriesByMeAttributes>, CategoriesByMeAttributes {}
+
+export default (sequelize: Sequelize) => {
+  const CategoriesByMeModel = sequelize.define<CategoriesByMeInstance>("CategoriesByMe", {
+    id: {
+      primaryKey: true,
+      type: DataTypes.STRING
+    },
+    serverID: {
+      type: DataTypes.STRING
+    },
+    channelID: {
+      type: DataTypes.STRING
+    }, name: {
+      type: DataTypes.STRING
+    }, 
+    userID: {
+      type: DataTypes.STRING
+    }
+  });
+  CategoriesByMeModel.sync();
+
+  return CategoriesByMeModel;
+};
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,16 @@
 import { Sequelize } from "sequelize";
 import WatchingVoiceChannel from "./watchingvoicechannel";
 import ChannelsByMe from "./channelsbyme";
+import CategoriesByMe from "./categoriesbyme";
 // const env = process.env.NODE_ENV || "development";
 // const config = require(__dirname + "/../config/config.json")[env];
 let sequelize = new Sequelize("sqlite::memory:", { logging: false });
+
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 export type AssociateFunction = (db: any) => void;
 
@@ -11,13 +18,8 @@ let db = {
   sequelize: sequelize,
   Sequelize: Sequelize,
   WatchingVoiceChannelModel: WatchingVoiceChannel(sequelize),
-  ChannelsByMeModel: ChannelsByMe(sequelize)
+  ChannelsByMeModel: ChannelsByMe(sequelize),
+  CategoriesByMeModel: CategoriesByMe(sequelize)
 };
-
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
 
 export default db;
